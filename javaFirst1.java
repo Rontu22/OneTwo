b1018c8ae7f9817f8f180b7b3788ca1334c4aa5c
Rontu22 <rontubarhoi01@gmail.com>
2020-12-31 13:17:11 +0530

    added one file

---
+++ javaFirst1.java
@@ -0,0 +1,11 @@
+public class javaFirst1
+{
+    public static void main(String args[])
+    {
+        System.out.println("This is another java program");
+        for(int i=0;i<90;i++)
+        {
+            System.out.println("*");
+        }
+    }
+}
